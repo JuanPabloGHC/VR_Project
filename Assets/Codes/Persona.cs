a6e4c3f13bccf340dbec9bcfe8d70f92807467d2
JuanPabloGHC <jpghc552@hotmail.com>
2022-11-04 12:19:57 -0600

    Cambio de escenas, texto de distancia al punto B

--- Assets/Codes/Persona.cs
+++ Assets/Codes/Persona.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Persona : MonoBehaviour
 {
     private bool onEndPoint;
     public GameObject panelIndicaciones;
+    public TMP_Text distanciaTexto;
+    public Transform puntoFinal;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +22,16 @@ public class Persona : MonoBehaviour
     {
         if (!onEndPoint)
         {
+            float dist = Vector3.Distance(puntoFinal.position, this.transform.position);
+            int distExacta = (int)dist;
+            distanciaTexto.text = distExacta.ToString() + " metros";
             panelIndicaciones.SetActive(true);
         }
         else
         {
+            distanciaTexto.text = " ";
             panelIndicaciones.SetActive(false);
+            SceneManager.LoadScene("SampleScene");
         }
     }
 
